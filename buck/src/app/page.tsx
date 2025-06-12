497cbe723201a97c2d83d2cf25da6bec0b2fc173
Carlo Jay O. Rivera <carlojay432@gmail.com>
2025-06-13 02:02:46 +0800

    [feature] added Title card

--- buck/src/app/page.tsx
+++ buck/src/app/page.tsx
@@ -7,10 +7,12 @@ export default function Home() {
   return (
     <div className="mainStrip">
       <div className="welcomeSign">
-      <img src="./BuckSign.png" alt="welcome Sign" id="sign"></img>
-      </div>
-      <div className="welcomeMsg">
-
+        <div className="buckmsg">
+        <p id="name">Buck</p>
+        <div className="smoothLine"></div>
+        <p id="desc">The Budget Tracker</p>
+        </div>
+        <div className="buckmascot"></div>
       </div>
     </div>
   );
