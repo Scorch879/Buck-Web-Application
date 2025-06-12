e1c70b9fe2e6def5325736607303b6316c9cf5d0
Carlo Jay O. Rivera <carlojay432@gmail.com>
2025-06-13 02:30:44 +0800

    [feature]->added incomplete button and welcome message

--- buck/src/app/page.tsx
+++ buck/src/app/page.tsx
@@ -5,15 +5,23 @@ import {Header,Footer} from '../component/HeaderFooter'
 
 export default function Home() {
   return (
+    <>
     <div className="mainStrip">
       <div className="welcomeSign">
         <div className="buckmsg">
-        <p id="name">Buck</p>
-        <div className="smoothLine"></div>
-        <p id="desc">The Budget Tracker</p>
+          <p id="name">Buck</p>
+          <div className="smoothLine"></div>
+          <p id="desc">The Budget Tracker</p>
         </div>
-        <div className="buckmascot"></div>
+          <div className="buckmascot"></div>
       </div>
+      <div className="welcomeMsg">  
+        <p>Hello World</p>
+      </div>
+      <button id="btnGetstarted">
+        <span>Get Started</span>
+      </button>
     </div>
+    </>
   );
 }
